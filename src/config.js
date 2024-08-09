660e4cc53f37c1d64af4af5f4d1edd99978c4128
Sct_jr <rishabh5400tiwari@gmail.com>
2024-08-10 01:48:45 +0530

    corrcection

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 // const BASE_URL = "https://ca-backend24.onrender.com"; // Global base URL here
 // const BASE_URL = "http://localhost:8080"; // Local URL here
-const BASE_URL = "https://ca-backend24-jxdl.onrender.com"; //Another global URL 
+const BASE_URL = "https://ca-backend24-jxdl.or.com"; //Another global URL 
 
 export default {
   BASE_URL,
