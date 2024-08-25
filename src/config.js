b19e8a436dd0ef05e71e18fa08d7223de4fbd150
Sct_jr <rishabh5400tiwari@gmail.com>
2024-08-25 10:55:48 +0530

    hosuygfg

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // const BASE_URL = "https://ca-backend24.onrender.com"; // Global base URL here
 // const BASE_URL = "http://localhost:8080"; // Local URL here
 // const BASE_URL = "https://ca-backend24-jxdl.onrender.com"; //Another global URL 
-const BASE_URL = "https://ca-backend24-jxdl.onrender.com"; //Another global URL 
+const BASE_URL = "https://axis-backend.onrender.com"; //Another global URL 
 
 export default {
   BASE_URL,
