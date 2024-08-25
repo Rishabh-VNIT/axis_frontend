36c403dbd547d9e143560adcab7688c685044ddb
Sct_jr <rishabh5400tiwari@gmail.com>
2024-08-25 13:47:49 +0530

    yegvjb

--- src/comp/Ab-aim/Ab-aim.js
+++ src/comp/Ab-aim/Ab-aim.js
@@ -11,7 +11,7 @@ const AboutAim = () => {
                 <h2 className="font-bold text-2xl lg:text-3xl text-center text-white">About Us</h2>
                 <div className="w-6 h-1 bg-green-500 mx-auto"></div>
                 <p className="text-lg lg:text-xl text-center leading-6 lg:leading-7 text-white">
-                The AXIS'24 Campus Ambassador program is designed for students who wish to actively participate in and promote the AXIS'24 technical fest on their college campuses. The Campus Ambassador program is an excellent opportunity for students to actively engage with AXIS'24, gain valuable event management and promotion experience, and contribute to the success of the technical fest on their campus.
+                The AXIS'25 Campus Ambassador program is designed for students who wish to actively participate in and promote the AXIS'25 technical fest on their college campuses. The Campus Ambassador program is an excellent opportunity for students to actively engage with AXIS'25, gain valuable event management and promotion experience, and contribute to the success of the technical fest on their campus.
                 </p>
               </div>
             </div>
